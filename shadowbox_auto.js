64dbc015c5f1271275eafe95165236c612beeee6
psynaptic <psynaptic>
2008-09-17 10:09:06 +0000

    Fixed issue with automatic image handling where existing rel attribute values would be overridden.

--- shadowbox_auto.js
+++ shadowbox_auto.js
@@ -1,10 +1,12 @@
-// $Id: shadowbox_auto.js,v 1.1.2.2 2008/09/17 09:21:13 psynaptic Exp $
+// $Id: shadowbox_auto.js,v 1.1.2.3 2008/09/17 10:09:06 psynaptic Exp $
 if (Drupal.jsEnabled) {
   $(document).ready(function() {
     var settings = Drupal.settings.shadowbox;
     if (settings.auto_enable_all_images == 1) {
-      // all image links enabled
-      $('a[href$=jpg], a[href$=png], a[href$=gif], a[href$=jpeg], a[href$=bmp]').attr('rel', 'shadowbox');
+      var images = 'a[href$=jpg][rel=""], a[href$=png][rel=""], a[href$=gif][rel=""], a[href$=jpeg][rel=""], a[href$=bmp][rel=""]';
+      $(images).each(function() {
+        $(this).attr('rel', 'shadowbox');
+      });
     };
   });
 }
