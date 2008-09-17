328c7a3ef6b696aedb9d80357f842f48a5cebd3b
psynaptic <psynaptic>
2008-09-17 09:21:13 +0000

    Whitespace clean-up.

--- shadowbox_auto.js
+++ shadowbox_auto.js
@@ -1,10 +1,10 @@
-// $Id: shadowbox_auto.js,v 1.1.2.1 2008/09/17 09:20:20 psynaptic Exp $
+// $Id: shadowbox_auto.js,v 1.1.2.2 2008/09/17 09:21:13 psynaptic Exp $
 if (Drupal.jsEnabled) {
   $(document).ready(function() {
-	  var settings = Drupal.settings.shadowbox;
-		if (settings.auto_enable_all_images == 1) {
-			// all image links enabled
-		  $('a[href$=jpg], a[href$=png], a[href$=gif], a[href$=jpeg], a[href$=bmp]').attr('rel', 'shadowbox');
-		};
+    var settings = Drupal.settings.shadowbox;
+    if (settings.auto_enable_all_images == 1) {
+      // all image links enabled
+      $('a[href$=jpg], a[href$=png], a[href$=gif], a[href$=jpeg], a[href$=bmp]').attr('rel', 'shadowbox');
+    };
   });
 }
