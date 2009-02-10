d5ca990697019b4798623cf3078c36412ec6ca47
psynaptic <psynaptic>
2009-02-10 17:02:41 +0000

    Adding additional option to display automatically handled images in a shadowbox gallery.

--- shadowbox_auto.js
+++ shadowbox_auto.js
@@ -1,11 +1,16 @@
-// $Id: shadowbox_auto.js,v 1.1.4.2 2008/11/05 00:07:12 psynaptic Exp $
+// $Id: shadowbox_auto.js,v 1.1.4.3 2009/02/10 17:02:41 psynaptic Exp $
 if (Drupal.jsEnabled) {
   $(document).ready(function() {
     var settings = Drupal.settings.shadowbox;
     if (settings.auto_enable_all_images == 1) {
       var images = 'a[href$=jpg][rel=""], a[href$=png][rel=""], a[href$=gif][rel=""], a[href$=jpeg][rel=""], a[href$=bmp][rel=""]';
       $(images).each(function() {
-        $(this).attr('rel', 'shadowbox');
+        if (settings.auto_gallery == 1) {
+          $(this).attr('rel', 'shadowbox[gallery]');
+        }
+        else {
+          $(this).attr('rel', 'shadowbox');
+        };
       });
     };
   });
